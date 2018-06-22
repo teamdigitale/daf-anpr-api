6b5714f532e2158e02289c26d83b9be2c7070bd3
Roberto Polli <robipolli@gmail.com>
2018-06-22 12:37:43 +0200

    WIP #8: add tests for public.

--- prj-simple/swagger_server/controllers/admins_controller.py
+++ prj-simple/swagger_server/controllers/admins_controller.py
@@ -4,23 +4,27 @@ import six
 from swagger_server.models.inline_response400 import InlineResponse400  # noqa: E501
 from swagger_server.models.table import Table  # noqa: E501
 from swagger_server.models.table_data import TableData  # noqa: E501
-from swagger_server import util, tools
+from swagger_server import util
 from swagger_server.controllers.public_controller import get_dictionary
-
+from swagger_server.tools import es, get_index_doctypes
 from connexion import problem
 
 from elasticsearch import Elasticsearch, helpers
 import json
-from datetime import datetime
+from datetime import datetime, timedelta
 
 
 def upload_dictionary(dictionary_name, body):  # noqa: E501
-    """Upload a new (version of a) dictionary eventually creating a new dictionary. The passed csv file contains a trailing line with the expected line count. If the schema does not match previous version, an error is returned. 
+    """Upload a new (version of a) dictionary eventually creating a new dictionary.
+    The passed csv file contains a trailing line with the expected line count.
+     If the schema does not match previous version, an error is returned.
 
      # noqa: E501
 
     :param dictionary_name: The name of the dictionary
     :type dictionary_name: str
+    :param index: The field to index
+    :type index: str
     :param body: Use MarkDown here!
     :type body: dict | bytes
 
@@ -30,33 +34,48 @@ def upload_dictionary(dictionary_name, body):  # noqa: E501
         return problem(status=415, title="Unsupported Media Type",
                        detail="Bad Content-Type: did you use application/json?")
     body = connexion.request.get_json()  # noqa: E501
-    print("body: ", body)
-    
-    es = Elasticsearch(hosts='elastic')
+    index = body['index']
+    description = body['description']
 
-    #if version == 'latest':
-    #    raise ValueError("""Version should not be "latest".""")
-    
-    versions = tools.get_index_doctypes(es, dictionary_name)
+    versions = get_index_doctypes(dictionary_name)
     new_version = datetime.now().isoformat()[:19]
     if versions and (versions[-1] > new_version):
         raise NotImplementedError(f"Version too low: {versions} vs {new_version}")
-    
-    loader = DataLoader('elastic', dictionary_name, new_version, 'codistat')
-    inserted, errors = loader.index_data(body)
-    return get_dictionary(dictionary_name)
+
+    loader = DataLoader(es, dictionary_name, new_version, index)
+
+    try:
+        inserted, errors = loader.index_data(body['items'])
+        print(f"inserted: {inserted}, errors: {errors}")
+    except KeyError:
+        return problem(status=500, title="Internal Server Error",
+                       detail=f"Error processing data: maybe index '{index}' not in items.")
+
+    # Add doctype metadata, including:
+    # - index
+    # - description
+    es.indices.put_mapping(index=dictionary_name, doc_type=new_version,
+                           body={
+                               '_meta': {
+                                   'index': index,
+                                   'description': description,
+                                   'ttl': datetime.now() + timedelta(days=60)
+                               }
+                           }
+                           )
+
+    return get_dictionary(dictionary_name), 201
 
 
 class DataLoader:
 
     """exposes the methods to index and update data from the cities"""
 
-    def __init__(self, hosts, index_name='anpr', doc_type='comuni', id_col_name='CODISTAT'):
-        self._hosts = hosts
+    def __init__(self, es, index_name='anpr', doc_type='comuni', id_col_name='CODISTAT'):
         self._index_name = index_name
         self._doc_type = doc_type
         self._id_col_name = id_col_name
-        self._es = Elasticsearch(hosts=hosts)
+        self._es = es
 
     def index_data(self, json_data):
         """ this method is called to index the information about the city
@@ -96,4 +115,3 @@ class DataLoader:
             '_id': id_doc,
             'doc': doc
         }
-
--- prj-simple/swagger_server/test/test_public_controller.py
+++ prj-simple/swagger_server/test/test_public_controller.py
@@ -1,40 +1,14 @@
 # coding: utf-8
 
-from __future__ import absolute_import
-
-from flask import json
-from six import BytesIO
-
-from swagger_server.models.problem import Problem  # noqa: E501
-from swagger_server.models.timestampa import Timestampa  # noqa: E501
-from swagger_server.test import BaseTestCase
+from swagger_server.test.harness import assert_dict_in, assert_keys_in, TestHarness
 
 
-def assert_keys_in(keys_, dict_):
-    k1, k2 = set(keys_), set(dict_)
-    assert k1 < k2, "Missing keys: %r" % (k1 - k2)
-
-
-def assert_dict_in(sub_, dict_):
-    delta = {k: dict_[k]
-             for k, v in sub_.items()
-             if dict_[k] != v}
-
-    assert not delta, "Missing items: %r" % delta
-
-
-class TestPublicController(BaseTestCase):
+class TestPublicController(TestHarness):
 
     """PublicController integration test stubs"""
 
-    def request(self, uri, method='GET', **kwargs):
-        response = self.client.open(uri, method=method, **kwargs)
-        self.assert200(response,
-                       'Response body is : ' + response.data.decode('utf-8'))
-        return response, json.loads(response.data.decode('utf-8'))
-
     def test_get_dictionaries(self):
-        response, data = self.request(
+        response, data = self.request(  # noqa:
             '/robipolli/core-vocabularies/0.0.1/dictionaries')
         assert_keys_in(('items', 'offset'), data)
 
@@ -60,6 +34,14 @@ class TestPublicController(BaseTestCase):
         v = data['last_version']
         response, data = self.request(
             f'/robipolli/core-vocabularies/0.0.1/dictionaries/comuni/{v}?limit=5&offset=5'
+        )
+        assert 'items' in data
+        assert len(data['items']) == 5
+        assert_dict_in({'offset': 5, 'offset_next': 10}, data)
+
+    def _test_get_dictionary_versions_latest(self):
+        response, data = self.request(
+            f'/robipolli/core-vocabularies/0.0.1/dictionaries/comuni/latest?limit=5&offset=5'
         )
         assert 'items' in data
         assert len(data['items']) == 5
